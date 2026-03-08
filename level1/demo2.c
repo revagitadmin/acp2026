2e2ce1853f45ccef920e124823270cd2c448c345
PavanTej <is.pavantejgamil.com>
2026-03-08 21:08:11 +0530

    1.spelling mistake of calculate,2.created another datatype similar to Rectangle but with different name rect and returned it which caused mismatch

---
+++ level1/demo2.c
@@ -0,0 +1,94 @@
+/*Write a C program to compare areas of three rectangles and find the largest 
+using functions and structure. (else-if ladder)
+typedef struct {
+    	char name[50];
+    	float length;
+    	float width;
+    	float area;
+} Rectangle;
+Function prototype:
+Rectangle input();
+float calculate_area(Rectangle rect);
+void compare_areas(Rectangle r1, Rectangle r2, Rectangle r3);*/
+#include<stdio.h>
+typedef struct{
+    char name[50];
+	float length;
+	float width;
+	float area;
+}Rectangle;
+Rectangle input(void);
+float claculate_area(Rectangle r);
+void compare_areas(Rectangle x,Rectangle y,Rectangle z);
+int main(){
+	Rectangle r1,r2,r3;
+	r1=input();
+	r2=input();
+	r3=input();
+	r1.area=calculate_area(r1);
+	r2.area=calculate_area(r2);
+	r3.area=calculate_area(r3);
+	compare_areas(r1,r2,r3);
+	return 0;
+}
+Rectangle input(void){
+	typedef struct{
+		char name[50];
+		float length,width,area;
+	}rect;
+	printf("Enter name of rectangle: ");
+	scanf("%s",rect.name);
+	printf("Enter length of rectangle: ");
+	scanf("%f",rect.length);
+	printf("Enter width of rectangle: ");
+	scanf("%f",rect.width);
+	return rect;
+}
+float calculate_area(Rectangle r){
+	r.area=r.length*r.width;
+	return r.area;
+}
+void compare_areas(Rectangle x,Rectangle y,Rectangle z){
+	if((x.area>y.area)&&(x.area>z.area)){
+		printf("%s has largest area\n",x.name);
+	}
+	else if((y.area>x.area)&&(y.area>z.area)){
+		printf("%s has largest area\n",y.name);
+	}
+	else if((z.area>x.area)&&(z.area>y.area)){
+		printf("%s has largest area\n",z.name);
+	}
+	else{
+		printf("Areas are equal\n");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
