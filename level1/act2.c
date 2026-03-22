7dffc10f45c4e2daa7e5c4307ac7bdd2f540c87a
Purva Vachanni <purvavachanni@gmail.com>
2026-03-22 13:39:25 +0530

    modi act2.c

---
+++ level1/act2.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+typedef struct {
+    int units;
+    float rate;
+    float total_bill;
+} ElectricityBill;
+
+ElectricityBill input();
+ElectricityBill calculate_bill(ElectricityBill bill);
+void output(ElectricityBill bill);
+
+int main() {
+    ElectricityBill bill;
+
+    bill = input();
+
+    bill = calculate_bill(bill);
+
+    output(bill);
+
+    return 0;
+}
+
+ElectricityBill input() {
+    ElectricityBill bill;
+
+    printf("Enter units consumed: ");
+    scanf("%d", &bill.units);
+
+    printf("Enter rate per unit: ");
+    scanf("%f", &bill.rate);
+
+    return bill;
+}
+ElectricityBill calculate_bill(ElectricityBill bill) {
+    bill.total_bill = bill.units * bill.rate;
+
+    if (bill.units > 100) {
+        int extra_units = bill.units - 100;
+        bill.total_bill += extra_units * 5;
+
+        printf("\nHigh consumption Extra charge applied\n");
+    }
+
+    return bill;
+}
+
+void output(ElectricityBill bill) {
+    printf("\nElectricity Bill Details:\n");
+    printf("Units Consumed: %d\n", bill.units);
+    printf("Rate per Unit: %.2f\n", bill.rate);
+    printf("Total Bill: ₹%.2f\n", bill.total_bill);
+}
