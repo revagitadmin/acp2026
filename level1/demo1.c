685a357464bf2532ceff1ed4c044d7ab71cc1a87
Purva Vachanni <purvavachanni@gmail.com>
2026-03-23 21:27:25 +0530

    modi demo1

--- level1/demo1.c
+++ level1/demo1.c
@@ -1,39 +1,44 @@
 #include <stdio.h>
-void input(int n,float arr[n])
+
+void input(int n, float arr[n])
 {
-    printf("enter the number of elements : \n");
-    scanf("%d",&n);
-    printf("enter the elments of the array :");
-    for(int i=0;i<n;i++)
+    for(int i = 0; i < n; i++)
     {
-        scanf("%f",&arr[i]);
+        scanf("%f", &arr[i]);
     }
 }
-int find_max_index(int n,float arr[n])
+
+int find_max_index(int n, float arr[n])
 {
-    int maxindex=0;
-    for(int i=1;i<n;i++)
+    int maxindex = 0;
+    for(int i = 1; i < n; i++)
     {
-        if(arr[i]>arr[maxindex])
+        if(arr[i] > arr[maxindex])
         {
-            maxindex=i;
+            maxindex = i;
         }
     }
     return maxindex;
 }
-void output(float arr[ ], int maxindex)
+
+void output(float arr[], int maxindex)
 {
     printf("Maximum value = %.2f\n", arr[maxindex]);
     printf("Index of maximum value = %d\n", maxindex);
 }
+
 int main()
 {
-    int max,n;
-    printf("Enter the number of elements: ");
+    int n;
     scanf("%d", &n);
+
     float a[n];
-    input(n,a);
-    max=find_max_index(n,a);
-    output(a,max);
+
+    input(n, a);
+
+    int max = find_max_index(n, a);
+
+    output(a, max);
+
     return 0;
 }
